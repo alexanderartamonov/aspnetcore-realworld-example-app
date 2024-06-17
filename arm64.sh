3fa3653f087f70e016d31dfb9f8b3baaeee78db4
Oleksandr Artamonov <oleksandrartamonov@Oleksandrs-MacBook-Pro-4.local>
2024-06-17 13:04:55 +0300

    update multiarch jenkins file

--- arm64.sh
+++ arm64.sh
@@ -1,8 +1,8 @@
 TRACER_VERSION=$1
-echo 'install dotnet tracer' && \
-mkdir -p /opt/datadog \
-&& mkdir -p /var/log/datadog \
-&& curl -LO https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb \
-&& dpkg -i ./datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb \
-&& /opt/datadog/createLogPath.sh \
-&& rm ./datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb
+echo "install dotnet tracer ${TARGETARCH}"
+mkdir -p /opt/datadog 
+mkdir -p /var/log/datadog 
+curl -LO https://github.com/DataDog/dd-trace-dotnet/releases/download/v${TRACER_VERSION}/datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb 
+dpkg -i ./datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb 
+/opt/datadog/createLogPath.sh 
+rm ./datadog-dotnet-apm_${TRACER_VERSION}_arm64.deb
